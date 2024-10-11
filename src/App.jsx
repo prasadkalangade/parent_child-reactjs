74fc1147ec1ee572e37cc1933dc5f5f0ab497fb5
prasad kalangade <prasadbkalangade@gmail.com>
2024-10-11 14:19:53 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Mine from './mine'
+function App() {
+  
+
+  return (
+    <>
+    
+      <Mine/>
+    </>
+  )
+}
+
+export default App
---
+++ src/mine.jsx
@@ -0,0 +1,14 @@
+function App() {
+  
+
+    return (
+      <>
+        <h1>hello</h1>
+        <h2>this is mine</h2>
+        
+      </>
+    )
+  }
+  
+  export default App
+  
